1380917a3a8709a624bc62e5f1db76ee3b189361
ShiftUnion <nick.cahill@gmail.com>
2016-12-17 21:43:31 +1300

    Got some serious bot flow action! Added vscode... looking pretty good

--- messages/tron.js
+++ messages/tron.js
@@ -5,67 +5,71 @@ require('dotenv').load();
 
 var useEmulator = (process.env.NODE_ENV == 'development');
 
-console.log("em:" + useEmulator);
-
 var connector = useEmulator ? new builder.ChatConnector() : new botbuilder_azure.BotServiceConnector({
-    appId: process.env['MicrosoftAppId'],
-    appPassword: process.env['MicrosoftAppPassword'],
-    stateEndpoint: process.env['BotStateEndpoint'],
-    openIdMetadata: process.env['BotOpenIdMetadata']
-  });
+  appId: process.env['MicrosoftAppId'],
+  appPassword: process.env['MicrosoftAppPassword'],
+  stateEndpoint: process.env['BotStateEndpoint'],
+  openIdMetadata: process.env['BotOpenIdMetadata']
+});
 
 var bot = new builder.UniversalBot(connector, [
-  function (session) {
-    session.beginDialog('q&aDialog');
-  },
-  function (session, results) {
-    session.send("Thanks %(name)s... You're %(age)s and located in %(state)s.", results.response);
-  }
 ]);
+
+
 
 bot.dialog('/', [
-  function (session) {
-    session.beginDialog('q&aDialog');
-  },
-]);
-
-// Add Q&A dialog
-bot.dialog('q&aDialog', [
   function (session, args) {
-    // Save previous state (create on first call)
-    session.dialogData.index = args ? args.index : 0;
-    session.dialogData.form = args ? args.form : {};
 
     // Prompt user for next field
-    builder.Prompts.text(session, questions[session.dialogData.index].prompt);
+    builder.Prompts.text(session, 'Are you ready to kick-off?');
   },
   function (session, results) {
-    // Save users reply
-    var field = questions[session.dialogData.index++].field;
-    session.dialogData.form[field] = results.response;
-
-    // Check for end of form
-    if (session.dialogData.index >= questions.length) {
-      // Return completed form
-      session.endDialogWithResult({ response: session.dialogData.form });
-    } else {
-      // Next field
-      session.replaceDialog('q&aDialog', session.dialogData);
+    console.log(results)
+    if (results.response == 'y') {
+      session.beginDialog('/motivate', 4);
     }
+  },
+    function (session, results) {
+      session.beginDialog('/journal')
+  },
+
+  function (session, results) {
+    session.send('thanks - dude, see you tomorrow')
+  }
+
+]);
+
+bot.dialog('/motivate', [
+  function moreThings(session, numRequested) {
+    if (numRequested == null) numRequested = 3;
+    for (let i = 0; i < numRequested; i++) {
+      session.send('boom - motivation ' + i);
+    }
+    builder.Prompts.choice(session, 'Do you want more, or are you ready to journal?', ['more', 'journal', 'quit']);
+  },
+  function (session, results, next) {
+    if (results.response.entity === 'more') session.beginDialog('/motivate', 2);
+    if (results.response.entity === 'quit') session.endDialog();
+    if (results.response.entity === 'journal') next();
+
   }
 ]);
 
-var questions = [
-  { field: 'name', prompt: "What's your name?" },
-  { field: 'age', prompt: "How old are you?" },
-  { field: 'state', prompt: "What state are you in?" }
-];
+bot.dialog('/journal', [
+  function (session) {
+    builder.Prompts.text(session, 'Please add a journal entry (todo)?');
+  },
+  function (session, results, next) {
+    session.send('thanks for you entry: ' + results.response)
+    next();
+  }
+]);
 
 
 if (useEmulator) {
   var restify = require('restify');
   var server = restify.createServer();
-  server.listen(3978, function() {
+  server.listen(3978, function () {
     console.log('test bot endpont at http://localhost:3978/api/messages');
   });
   server.post('/api/messages', connector.listen());
